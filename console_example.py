85b913dd099e754a204cd11935fca6af20b9ed22
johnlk <jlkuhn@purdue.edu>
2019-11-09 11:09:43 -0500

    added functions for weather and the current time to display

--- console_example.py
+++ console_example.py
@@ -1,5 +1,7 @@
 import time
 from char_mappings import get_mapping
+from datetime import datetime
+import requests
 
 screen = [
 '0000000000000000000000000000000000000000000000000000000000000',
@@ -38,11 +40,30 @@ def add_message(message):
 		screen[4] + message_mapping[4]
 	]
 
-add_message('women wear the worse wooley whiskers.')
+def get_time():
+  return datetime.now().strftime('%a-%d   %I:%M %p')
+
+def kelvin_to_far(float_val):
+  celcius = float_val - 273.15
+  return str(int(celcius * (9/5) + 32))
+
+def get_weather():
+  #response = requests.get("api.openweathermap.org/data/2.5/weather?zip=47905,us&" +
+  #  "appid=5c48857cbe6c1763f27742ae12bd805f")
+  response = requests.get("https://samples.openweathermap.org/data/2.5/weather?zip=94040,us&appid=b6907d289e10d714a6e88b30761fae22")
+  if response.status_code != 200:
+    return "404 weather"
+
+  response = response.json()
+  #print(response)
+
+  weather = kelvin_to_far(response['main']['temp']) + " degrees, " + response['weather'][0]['description']
+  return weather
+
+add_message(get_weather())
 
 while True:
 	color = (0, 0, 255)
 	shiftLeft(1)
 	fillScreen()
 	time.sleep(0.08)
- 
--- name.py
+++ name.py
@@ -1,7 +1,9 @@
 import time
 import board
 import neopixel
+import requests
 from char_mappings import get_mapping 
+from datetime import datetime
 
 from pubnub.enums import PNStatusCategory
 from pubnub.pnconfiguration import PNConfiguration
@@ -81,9 +83,29 @@ def add_message(message):
 		screen[4] + message_mapping[4]
 	]
 
+def get_time():
+  return datetime.now().strftime('%a-%d   %I:%M %p')
+
+def kelvin_to_far(float_val):
+  celcius = float_val - 273.15
+  return str(int(celcius * (9/5) + 32))
+
+def get_weather():
+  #response = requests.get("api.openweathermap.org/data/2.5/weather?zip=47905,us&" +
+  #  "appid=5c48857cbe6c1763f27742ae12bd805f")
+  response = requests.get("https://samples.openweathermap.org/data/2.5/weather?zip=94040,us&appid=b6907d289e10d714a6e88b30761fae22")
+  if response.status_code != 200:
+    return "404 weather"
+
+  response = response.json()
+  #print(response)
+
+  weather = kelvin_to_far(response['main']['temp']) + " degrees, " + response['weather'][0]['description']
+  return weather
+
+
 clear()
 pixels.show()
-
 
 while True:
 	result = my_listener.wait_for_message_on('ledWall')
